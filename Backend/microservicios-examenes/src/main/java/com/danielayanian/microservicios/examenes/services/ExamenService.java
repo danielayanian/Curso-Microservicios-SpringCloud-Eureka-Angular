be0bdba308228e4b26db575fe6119496b8a05380
Daniel Ayanian <48890153+danielayanian@users.noreply.github.com>
2023-09-11 17:36:32 -0300

    agregando microservicio examenes

---
+++ Backend/microservicios-examenes/src/main/java/com/danielayanian/microservicios/examenes/services/ExamenService.java
@@ -0,0 +1,8 @@
+package com.danielayanian.microservicios.examenes.services;
+
+import com.danielayanian.microservicios.commons.services.CommonService;
+import com.danielayanian.microservicios.examenes.models.entity.Examen;
+
+public interface ExamenService extends CommonService<Examen> {
+
+}
