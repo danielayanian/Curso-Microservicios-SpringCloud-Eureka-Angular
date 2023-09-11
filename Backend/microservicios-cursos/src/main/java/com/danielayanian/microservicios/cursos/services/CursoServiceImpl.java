a846bbf28de021d8afae10ccdaa3aa39d1acd7dc
Daniel Ayanian <48890153+danielayanian@users.noreply.github.com>
2023-09-11 17:08:07 -0300

    agregando el microservicio examenes

--- Backend/microservicios-cursos/src/main/java/com/danielayanian/microservicios/cursos/services/CursoServiceImpl.java
+++ Backend/microservicios-cursos/src/main/java/com/danielayanian/microservicios/cursos/services/CursoServiceImpl.java
@@ -1,6 +1,7 @@
 package com.danielayanian.microservicios.cursos.services;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.danielayanian.microservicios.commons.services.CommonServiceImpl;
 import com.danielayanian.microservicios.cursos.models.entity.Curso;
@@ -9,4 +10,12 @@ import com.danielayanian.microservicios.cursos.models.repository.CursoRepository
 @Service
 public class CursoServiceImpl extends CommonServiceImpl<Curso, CursoRepository> implements CursoService {
 
+	@Override
+	@Transactional(readOnly = true)
+	public Curso findCursoByAlumnoId(Long id) {
+		return repository.findCursoByAlumnoId(id);
+	}
+
+	
+	
 }
--- Backend/microservicios-usuarios/src/main/java/com/danielayanian/microservicios/usuarios/services/AlumnoService.java
+++ Backend/microservicios-usuarios/src/main/java/com/danielayanian/microservicios/usuarios/services/AlumnoService.java
@@ -1,9 +1,12 @@
 package com.danielayanian.microservicios.usuarios.services;
 
+import java.util.List;
+
 import com.danielayanian.microservicios.commons.alumnos.models.entity.Alumno;
 import com.danielayanian.microservicios.commons.services.CommonService;
 
 public interface AlumnoService extends CommonService<Alumno> {
 
+	public List<Alumno> findByNombreOrApellido(String term);
 
 }
