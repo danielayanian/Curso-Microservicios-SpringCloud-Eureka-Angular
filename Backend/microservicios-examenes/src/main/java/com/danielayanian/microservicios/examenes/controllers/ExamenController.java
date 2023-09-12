b46bebe3b8f47f85c27c132473a731e5b0444d58
danielayanian <48890153+danielayanian@users.noreply.github.com>
2023-09-11 23:37:18 -0300

    actualizacion de ExamenController

--- Backend/microservicios-examenes/src/main/java/com/danielayanian/microservicios/examenes/controllers/ExamenController.java
+++ Backend/microservicios-examenes/src/main/java/com/danielayanian/microservicios/examenes/controllers/ExamenController.java
@@ -1,5 +1,10 @@
 package com.danielayanian.microservicios.examenes.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -8,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.danielayanian.microservicios.commons.controllers.CommonController;
 import com.danielayanian.microservicios.examenes.models.entity.Examen;
+import com.danielayanian.microservicios.examenes.models.entity.Pregunta;
 import com.danielayanian.microservicios.examenes.services.ExamenService;
 
 @RestController
@@ -15,7 +21,35 @@ public class ExamenController extends CommonController<Examen, ExamenService> {
 
 	@PutMapping("/{id}")
 	public ResponseEntity<?> editar(@RequestBody Examen examen, @PathVariable Long id){
-		return null;
+		Optional<Examen> o = service.findById(id);
+		
+		if(!o.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		Examen examenDB = o.get();
+		examenDB.setNombre(examen.getNombre());
+		
+		List<Pregunta> eliminadas = new ArrayList<>();
+		examenDB.getPreguntas().forEach(pdb -> {
+			if(!examen.getPreguntas().contains(pdb)) {
+				eliminadas.add(pdb);
+			}
+		});
+		eliminadas.forEach(p -> {
+			examenDB.removePregunta(p);
+		});
+		
+		//Lo anterior lo podemos resumir asi usando programacion funcional
+		//examenDB.getPreguntas()
+		//.stream()
+		//.filter(pdb -> !examen.getPreguntas().contains(pdb))
+		//.forEach(examenDB::removePregunta);
+		
+		examenDB.setPreguntas(examen.getPreguntas());
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(service.save(examenDB));
+		
 	}
 	
 }
