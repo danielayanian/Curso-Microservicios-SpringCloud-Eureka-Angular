cbe59fcf1ab512dd49f5441c3b53f41e1d18fe8d
Daniel Ayanian <48890153+danielayanian@users.noreply.github.com>
2023-09-30 18:59:43 -0300

    creacion de componentes y entidades en Angular

--- Angular/cursos/src/app/app-routing.module.ts
+++ Angular/cursos/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { CursosComponent } from './components/cursos/cursos.component';
 import { ExamenesComponent } from './components/examenes/examenes.component';
 
 const routes: Routes = [
+  {path:"", pathMatch: 'full', redirectTo: 'cursos'},//Si no indico ruta, solo la raiz, se vera el componente cursos
   {path:"alumnos", component:AlumnosComponent},
   {path:"cursos", component:CursosComponent},
   {path:"examenes", component:ExamenesComponent}
